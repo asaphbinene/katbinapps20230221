5dab5b9a1b24ac8179b48ea99a2eb8c87795e7db
Binene Asaph Kalala <asaph@binene.com>
2023-02-21 21:44:36 +0200

    Add project files.

---
+++ katbinapps20230221/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace katbinapps20230221.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
